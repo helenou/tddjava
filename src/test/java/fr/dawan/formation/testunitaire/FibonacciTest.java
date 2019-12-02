934de06de34c78e8aa44e2371eb426e530453b64
helenou <ln.lejeune@gmail.com>
2019-12-02 16:52:49 +0100

    Using parameters

--- src/test/java/fr/dawan/formation/testunitaire/FibonacciTest.java
+++ src/test/java/fr/dawan/formation/testunitaire/FibonacciTest.java
@@ -1,21 +1,43 @@
 package fr.dawan.formation.testunitaire;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class FibonacciTest {
 
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			{0, 0},
+			{1, 1},
+			{2, 1},
+			{3, 2},
+			{4, 3},
+			{5, 5},
+			{6, 8},
+			{7, 13}			
+		});
+	}
+	
+	@Parameter(0) // param en index 0
+	public int donneeEnEntree;
+	@Parameter(1) // param en index 1
+	public int donneeEnSortie;
+	
 	
 	@Test
 	public void test() {
-		assertEquals(1, Calculatrice.fibonacci(1));
-		assertEquals(1, Calculatrice.fibonacci(2));
-		assertEquals(2, Calculatrice.fibonacci(3));
-		assertEquals(3, Calculatrice.fibonacci(4));
-		assertEquals(5, Calculatrice.fibonacci(5));
-		assertEquals(8, Calculatrice.fibonacci(6));
-		assertEquals(13, Calculatrice.fibonacci(7));
+		assertEquals(donneeEnSortie, Calculatrice.fibonacci(donneeEnEntree));
+
 	}
 
 }
