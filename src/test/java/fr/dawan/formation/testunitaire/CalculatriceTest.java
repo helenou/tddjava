5e1d9f723bcaa2450db2e02efae848d9594169a2
helenou <ln.lejeune@gmail.com>
2019-12-02 11:01:59 +0100

    first commit

---
+++ src/test/java/fr/dawan/formation/testunitaire/CalculatriceTest.java
@@ -0,0 +1,14 @@
+package fr.dawan.formation.testunitaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatriceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
